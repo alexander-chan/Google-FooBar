24c1a613840f1cd90ed7e596cfd1db198ee37833
Alexander Chan <alexander-chan@users.noreply.github.com>
2017-08-12 23:58:45 -0700

    Update Solution.py
    Edge cases supported

--- Challenge 2/Power Hungry/Solution.py
+++ Challenge 2/Power Hungry/Solution.py
@@ -7,6 +7,13 @@ def answer(xs):
     pos = [i for i in xs if i >= 1]
     neg.sort(reverse = True)
     prod = 1
+    #Edge cases
+    if len(pos) == 0 and 0 in xs:
+        return(str(0))
+    if len(neg) == 1 and len(pos) == 0:
+        return(str(neg[0]))
+        
+    #Main Solution
     if len(neg) > 1:
         if len(neg) % 2 == 0:
             prod = prod * reduce(lambda x, y: x*y, neg) 
@@ -15,20 +22,4 @@ def answer(xs):
     if len(pos) >=  1:
         prod = prod * reduce(lambda x, y: x*y, pos)
         
-    ### If no  > 1 solutions available, 
-    if len(neg) <= 1 and len(pos) < 1:
-        negDEC = [i for i in xs if i < 0 and i > -1]
-        posDEC = [i for i in xs if i > 0 and i < 1]
-        if len(negDEC) > 1:
-            negDEC.sorted()
-            negSol = negDEC[0] * negDEC[1]
-        if len(posDEC) > 0:
-            posDEC.sort(reverse = True)
-            posSol = posDEC[0]
-        if len(negDEC) > 1 and len(posDEC) > 0:
-            prod = negSol if negSol > posSol else posSol
-        elif len(negDEC) > 1:
-            prod = negSol
-        elif len(posDEC) > 0:
-            prod = posSol
     return(str(prod))
